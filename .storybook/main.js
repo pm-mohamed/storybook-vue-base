1996ae88a20773da0bcb9a32eead03e6f9919601
Mohamed Hassan <mohamed.hassan@pmagentur.com>
2022-03-11 13:38:16 +0100

    added sass loader to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,14 @@ module.exports = {
   stories: ["../src/stories/**/*.story.@(js|mdx)"],
   logLevel: "debug",
   addons: [
+    {
+        name: '@storybook/preset-scss',
+        options: {
+          cssLoaderOptions: {
+             modules: true,
+          },
+        }
+    },
     "@storybook/addon-docs",
     "@storybook/addon-controls",
     "@storybook/addon-storysource",
@@ -11,6 +19,8 @@ module.exports = {
     "@storybook/addon-viewport",
     "@storybook/addon-backgrounds",
     "@storybook/addon-a11y",
+    "@storybook/addon-essentials",
+    '@whitespace/storybook-addon-html',
   ],
   core: {
     builder: "webpack4",
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import '../src/App.scss'
 
 Vue.use(Vuex);
 
